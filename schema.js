5dcb29ff8f7b24422431ab9b0546903eca197aca
mohamed-saad <mahameds19999@gmail.com>
2019-01-16 11:56:38 +0200

    set the root query and export it

--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
-const {GraphQLOpjectType, GraphQLInt, GraphQLBoolean, GraphQLString} = require('graphql');
+const axios = require('axios');
+const {GraphQLOpjectType, GraphQLInt, GraphQLBoolean, GraphQLString, GraphQLList, GraphQLSchema} = require('graphql');
 
 // Launch type
 let LanchType = new GraphQLOpjectType({
@@ -21,4 +22,21 @@ let RocketType = new GraphQLOpjectType({
         rocket_name: {type: GraphQLString},
         rocket_type: {type: GraphQLString}
     })
+});
+
+let RootQuery = new GraphQLOpjectType({
+    name: "RootQueryType",
+    fields: {
+        launches: {
+            type: GraphQLList(LanchType),
+            resolve(parents, args) {
+                axios.get('https://api.spacexdata.com/v3/launches')
+                    .then(res => res.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
