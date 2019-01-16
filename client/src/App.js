0fe515740eac35018e2d59343a54d7152fa20e20
mohamed-saad <mahameds19999@gmail.com>
2019-01-16 13:47:52 +0200

    setup apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.png';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'https://localhost:4000'
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} alt="spacex logo"  style={{width: 300, display: 'block', margin:'auto'}}/>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <img src={logo} alt="spacex logo"  style={{width: 300, display: 'block', margin:'auto'}}/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
