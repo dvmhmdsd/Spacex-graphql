51c0d2cd77f5dc66953a855f4e2a0c1b37e74519
mohamed-saad <mahameds19999@gmail.com>
2019-01-16 11:45:59 +0200

    set the launch type

---
+++ schema.js
@@ -0,0 +1,14 @@
+const {GraphQLOpjectType, GraphQLInt, GraphQLBoolean, GraphQLString} = require('graphql');
+
+// Launch type
+let LanchType = new GraphQLOpjectType({
+    name: "Lanch",
+    fields: () => ({
+        flight_number: {type: GraphQLInt},
+        mission_name: {type: GraphQLString},
+        launch_year: {type: GraphQLString},
+        launch_date_local: {type: GraphQLString},
+        launch_success: {type: GraphQLBoolean},
+        rocket: {type: RocketType}
+    })
+});
