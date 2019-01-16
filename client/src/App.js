a17fd02f313cc140a5efd7e0e96f05eceeb152cf
mohamed-saad <mahameds19999@gmail.com>
2019-01-16 13:59:34 +0200

    add launches component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import logo from './logo.png';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { Launches } from './components/launches';
 
 const client = new ApolloClient({
   uri: 'https://localhost:4000'
@@ -12,8 +13,9 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
+        <div className="container">
           <img src={logo} alt="spacex logo"  style={{width: 300, display: 'block', margin:'auto'}}/>
+          <Launches />
         </div>
       </ApolloProvider>
     );
